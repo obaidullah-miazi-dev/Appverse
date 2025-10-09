bd3374a9ef6d878a0bfd4cd979259db1254879f6
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-10-09 21:42:02 +0600

    loading animation changed

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -3,8 +3,9 @@ import logoicon from '../assets/navbar-logo-icon.png'
 
 const Loader = () => {
     return (
-        <div className='h-screen flex justify-center items-center'>
-            <img className='animate-spin w-96' src={logoicon} alt="logo icon loading" />
+        <div className='h-screen flex justify-center items-center gap-5'>
+            <img className='animate-spin w-64' src={logoicon} alt="logo icon loading" />
+            <h1 className='text-8xl font-black text-[#ad0eb0] animate-pulse'>Loading</h1>
         </div>
     );
 };
